f529d455eee9dac14bb46d851e2e7b1da5b7de83
Yan Manica Oehling <yanmano@gmail.com>
2024-10-24 22:28:16 -0300

    atualizacoes estruturais

--- casa_de_cambio_dso/main.py
+++
@@ -1,4 +0,0 @@
-from controladores.inicializador import Inicializador
-
-if __name__ == "__main__":
-    Inicializador().inicializa_sistema()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from programacao.controlador.inicializador import Inicializador
+from controladores.inicializador import Inicializador
 
 if __name__ == "__main__":
     Inicializador().inicializa_sistema()
