f141b4176fcbe0587a4c77c62ea1b27be2bfe373
Yan Manica Oehling <125397533+yanoehling@users.noreply.github.com>
2024-12-02 21:36:53 -0300

    Add files via upload

--- excecoes.py
+++ excecoes.py
@@ -5,5 +5,20 @@ class OpcaoDigitadaIncorretamente(Exception):
 
 class ValorNaoNumerico(Exception):
     def __init__(self):
-        self.msg = 'Esse valor não é numérico'
+        self.msg = 'O valor digitado não é numérico'
+        super().__init__(self.msg)
+
+class NomeComDigito(Exception):
+    def __init__(self):
+        self.msg = f'Não pode haver números ou dígitos no nome'
+        super().__init__(self.msg)
+
+class ComCaractere(Exception):
+    def __init__(self, tipo_correto):
+        self.msg = f'Só pode haver {tipo_correto} na identidade do Cliente'
+        super().__init__(self.msg)
+
+class NaoInteiro(Exception):
+    def __init__(self, var):
+        self.msg = f'O valor de {var} não é um número, ou não é um Inteiro'
         super().__init__(self.msg)
